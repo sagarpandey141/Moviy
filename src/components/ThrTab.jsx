49a8f6085ac09af503e19987e9844354aa55c23a
sagarpandey141 <121431268+sagarpandey141@users.noreply.github.com>
2023-08-20 13:18:58 +0530

    what popular section

--- src/components/ThrTab.jsx
+++ src/components/ThrTab.jsx
@@ -6,7 +6,10 @@ const ThrTab = () => {
 
     const[days,setDays]=useState([]);
     const[week,setWeek]=useState([]);
+    const[PopularMovie,setPopularMovie]=useState([]);
+    const[PopularTvShow,setPopularTvShow]=useState([]);
     const[currentClick,setCurrentClick]=useState("days");
+    const[currentClickWhat,setCurrentClickWhat]=useState("movie");
     const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL;
     const BASE_URL=import.meta.env.VITE_BASE_URL;
 
@@ -25,44 +28,64 @@ const ThrTab = () => {
          }
     }
 
+    async function fetchPopularMovie(){
+       const response=await apiConnector("GET",BASE_URL+"/movie/popular?language=en-US&page=1")
+       if(response){
+           setPopularMovie(response?.data?.results);
+       }
+    }
+
+    async function fetchPopularTvShow(){
+       const response=await apiConnector("GET",BASE_URL+"/tv/popular?language=en-US&page=1")
+       console.log("res",response);
+       if(response){
+        setPopularTvShow(response?.data?.results);
+       }
+    }
+
     useEffect(()=>{
 
         fetchMovieDay();
         fetchMovieBaseOnWeek();
+        fetchPopularMovie();
+        fetchPopularTvShow();
 
     },[])
-    console.log("days",days);
-    console.log("week",week);
+     
+    console.log("tvshow",PopularTvShow);
   return (
     <div className=' text-white'>
-      
+        
+        {/*trending*/}
+
          <div className=' flex-col '>
               {/*2 button*/}
-              
-                <div className='flex   justify-around'>
-                 <h1 className=' text-3xl -translate-x-32'>Trending</h1>
-                 <div  className=' flex gap-3   bg-white  w-32  justify-center cursor-pointer p-3 text-black rounded-full'>
-                   <div className={`${currentClick==="days" ? " text-orange-500" : " text-black "}`} onClick={()=>setCurrentClick("days")}>Day</div>
-                   <div className={`${currentClick==="months" ? "  text-orange-500" : " text-black "}`} onClick={()=>setCurrentClick("months")}>Week</div>
+              <div className='  w-[72%] mx-auto'>
+                  <div className='flex    justify-between   '>
+                          <h1 className=' text-3xl '>Trending</h1>
+                  <div  className=' flex gap-3   bg-white  w-32  justify-center cursor-pointer p-3 text-black rounded-full'>
+                           <div className={`${currentClick==="days" ? " text-orange-500" : " text-black "}`} onClick={()=>setCurrentClick("days")}>Day</div>
+                           <div className={`${currentClick==="months" ? "  text-orange-500" : " text-black "}`} onClick={()=>setCurrentClick("months")}>Week</div>
+                  </div>
                   </div>
               </div>
 
               {/*card*/}
               {
                   currentClick==="days" ? (
-                    <div className='flex justify-center gap-3  mt-6'>
+                    <div className='flex justify-center gap-5  mt-6'>
                           {
                               days?.slice(0,5)?.map((data,index)=>(
                                  <div key={index} >
                                          {/*img*/}
-                                         <img className=' h-[19rem]  w-52 rounded-xl' src={IMAGE_BASE_URL+"w400"+data?.backdrop_path}/>
+                                         <img className=' h-[19rem]  w-52 rounded-xl' src={IMAGE_BASE_URL+"w400"+data?.poster_path}/>
 
                                          {/*genre*/}
                                          
                                          {/*round*/}
                                     
                                          {/*name*/}
-                                        <p> {data?.original_title && (
+                                        <p className=' text-lg'> {data?.original_title && (
                                             data.original_title.length > 13
                                               ? `${data.original_title.split(' ').slice(0, 3).join(' ')}...`
                                               : data.original_title
@@ -80,7 +103,7 @@ const ThrTab = () => {
                             week?.slice(0,5)?.map((data,index)=>(
                                   <div key={index} >
                                           {/*img*/}
-                                          <img  className=' h-[19rem]  w-52 rounded-xl' src={IMAGE_BASE_URL+"w400"+data?.backdrop_path}/>
+                                          <img  className=' h-[19rem]  w-52 rounded-xl' src={IMAGE_BASE_URL+"w400"+data?.poster_path}/>
 
                                           {/*genre*/}
                                           
@@ -102,7 +125,82 @@ const ThrTab = () => {
                   )
               }
          </div>
-    </div>
+          
+          {/*what popular*/}
+
+          <div className=' mt-10'>
+                    {/*2 button*/}
+              <div className='  w-[72%] mx-auto'>
+                  <div className='flex    justify-between   '>
+                          <h1 className=' text-3xl '>What's Popular</h1>
+                      <div  className=' flex gap-3   bg-white  w-32  justify-center cursor-pointer p-3 text-black rounded-full'>
+                              <div className={`${currentClickWhat==="movie" ? " text-orange-500" : " text-black "}`} onClick={()=>setCurrentClickWhat("movie")}>Movie</div>
+                              <div className={`${currentClickWhat==="tvshow" ? "  text-orange-500" : " text-black "}`} onClick={()=>setCurrentClickWhat("tvshow")}>TvShow</div>
+                      </div>
+                  </div>
+             </div>
+
+             {/*card*/}
+             <div>
+                   {
+                      currentClickWhat==="movie"? (
+                         <div  className='flex justify-center gap-5  mt-6'>
+                                {
+                                  PopularMovie.slice(0,5).map((data,index)=>(
+                                       <div key={index}>
+                                                 {/*img*/}
+                                         <img className=' h-[19rem]  w-52 rounded-xl' src={IMAGE_BASE_URL+"w400"+data?.poster_path}/>
+
+                                          {/*genre*/}
+
+                                          {/*round*/}
+
+                                          {/*name*/}
+                                              <p className=' text-lg'> {data?.original_title && (
+                                                data.original_title.length > 13
+                                                  ? `${data.original_title.split(' ').slice(0, 3).join(' ')}...`
+                                                  : data.original_title
+                                              )}</p>
+
+                                               {/*date*/}
+                                           <p>{data?.release_date}</p>
+
+                                       </div>
+                                  ))
+                                }
+                         </div>
+                      ) : (
+                         <div className='flex justify-center gap-5  mt-6'>
+                                {
+                                    PopularTvShow.slice(0,5).map((data,index)=>(
+                                         <div key={index}>
+
+                                                        {/*img*/}
+                                         <img className=' h-[19rem]  w-52 rounded-xl' src={IMAGE_BASE_URL+"w400"+data?.poster_path}/>
+
+                                                       {/*genre*/}
+
+                                                       {/*round*/}
+
+                                                       {/*name*/}
+                                              <p className=' text-lg'> {data?.overview && (
+                                                data.overview.length > 13
+                                                  ? `${data.overview.split(' ').slice(0, 3).join(' ')}...`
+                                                  : data.overview
+                                              )}</p>
+
+                                                  {/*date*/}
+                                              <p>{data?.first_air_date}</p>
+
+                                         </div>
+                                    ))
+                                }
+                         </div>
+                      )
+                   }
+             </div>
+          </div>
+    </div> 
   )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { apiConnector } from "../sevices/axios";
+import ThrTab from "../components/ThrTab";
 
 const HomePage = ({searchWord,setSearchWord}) => {
     const navigate = useNavigate();
@@ -21,14 +22,14 @@ async function backImages(){
 }
 
   return (
-    <div>
+    <div className="bg-[#08172f]">
     {/* bg image */}
-     <div className="relative bg-sky-300">
+     <div className="relative  ">
      <div className="absolute inset-0">
      </div>
       <div className="max-w-4xl h-[700px] mx-auto relative flex justify-center items-center w-11/12">
           <div className="flex flex-col w-full items-center">
-               <h1 className="text-7xl text-gray-700 font-bold">Welcome.</h1>
+               <h1 className="text-7xl text-white bold font-bold">Welcome.</h1>
                <p className=" text-center text-xl text-gray-400">Millions of movies, TV shows and people to discover. Explore now.</p>
                <div className="flex text-lg mt-10 max-w-xl w-10/12 ">
                <input type="text" value={searchWord} onChange={(e) => 
@@ -52,7 +53,7 @@ async function backImages(){
      </div>
 
      {/* sagar write your code below this line */}
-
+      <ThrTab/>
     </div>
   );
 };
